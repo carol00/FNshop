6251a99cc52ee1164cc2ac74b4b7045893cc4bc6
Carol <carol@Carolde-MacBook-Pro.local>
2021-08-10 18:33:15 +0800

    新增單一張商品展示layout

---
+++ src/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="banner">
+      <div class="picArea">
+        <img src="../assets/banner1.jpg">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {}
+}
+</script>
+
+<style scoped>
+@import '../style.css';
+
+.banner {
+    width: 100%;
+    margin: 40px 0;
+}
+
+.picArea {
+    width: 100%;
+    margin: auto;
+    max-width: 850px;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Search/>
+    <Banner/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Search from '@/components/Search.vue'
+import Search from '@/components/Search.vue';
+import Banner from '@/components/Banner.vue';
 
 export default {
   name: 'Home',
   components: {
-    Search
+    Search,
+    Banner
   }
 }
 </script>
