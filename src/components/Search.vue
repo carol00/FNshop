b7e3ae9166dfc9c3b002c6b0878a91928d6c423d
Carol <carol@Carolde-MacBook-Pro.local>
2021-08-10 16:39:13 +0800

    新增搜尋ui

---
+++ src/components/Search.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="search center">
+    <div class="searchInput">
+      <input type="search" placeholder="search">
+      <i class="fas fa-times-circle"></i>
+    </div>
+    <div class="searchBtn center">
+      <button><i class="fas fa-search"></i></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {}
+}
+</script>
+
+<style scoped>
+  @import '../style.css';
+
+.search {
+  width: 100%;
+  margin: 10px 5px;
+}
+
+/* input */
+.searchInput {
+  position: relative;
+  flex: 0 60%;
+}
+
+.searchInput input {
+  width: 100%;
+  height: 35px;
+  border: 2px solid var(--searchBorderColor);
+  border-top-left-radius: 100px;
+  border-bottom-left-radius: 100px;
+}
+
+.searchInput svg {
+  position: absolute;
+  right: 7px;
+  top: 10px;
+  color: #cecbcb;
+  font-size: 15px;
+}
+
+/* btn */
+.searchBtn button {
+  width: 45px;
+  height: 35px;
+  border-top-right-radius: 100px;
+  border-bottom-right-radius: 100px;
+  background: var(--searchBorderColor);
+  color: #FFF;
+  border: 2px solid var(--searchBorderColor);
+  font-size: 18px;
+}
+
+.searchBtn svg {
+  margin-right: 3px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld/>
+    <Search/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Search from '@/components/Search.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Search
   }
 }
 </script>
