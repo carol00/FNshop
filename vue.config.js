bf384f9f6c687137f227bb4c95bc40a7b8627d69
Carol <carol@Carolde-MacBook-Pro.local>
2021-08-11 01:42:14 +0800

    [修改] 網頁title、icon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    pages: {
+        index: {
+          // entry for the page
+          entry: 'src/main.js',
+          title: 'FN Shop',
+        },
+    },
     publicPath: process.env.NODE_ENV === 'production'
       ? '/FNshop/'
       : '/'
