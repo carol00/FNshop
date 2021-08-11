71afd44c4e7a34d162d0ea70e65eb6fc0c478501
Carol <carol@Carolde-MacBook-Pro.local>
2021-08-12 02:53:02 +0800

    [新增] nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="nav">
+    <ul v-for="value in item" :key="value">
+      <li><i :class="value.icon"></i><p class="amount">12</p></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Nav',
+  data() {
+    return {
+      item: [
+        { name: '購物車', icon: 'fas fa-shopping-cart' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.nav {
+  width: 100%;
+  height: 35px;
+  padding: 7px 17px;
+  background: #f3f0f0;
+}
+
+ul {
+  text-align: right;
+  color: #848383;
+  font-size: 20px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: relative;
+}
+
+.amount {
+  width: 18px;
+  height: 18px;
+  position: absolute;
+  background: #cc6241;
+  border-radius: 50%;
+  border: 1px #FFF solid;
+  top: -6px;
+  right: -9px;
+  font-size: 12px;
+  color: #FFF;
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Nav/>
     <Header/>
     <Banner/>
     <div class="line"></div>
@@ -11,13 +12,15 @@
 import Banner from '@/components/Banner.vue';
 import Content from '@/components/Content.vue';
 import Header from '@/components/Header.vue';
+import Nav from '@/components/Nav.vue';
 
 export default {
   name: 'Home',
   components: {
     Header,
     Banner,
-    Content
+    Content,
+    Nav
   }
 }
 </script>
