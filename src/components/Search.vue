44cc9b348473a90764a1efa5ea358945b5db8981
carol00 <carol1330s@gmaol.com>
2021-08-15 17:10:29 +0800

    [新增] 類別搜尋ui

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="search center">
+    <select>
+      <option v-for="item in prdType" :key="item">{{ item }}</option>
+    </select>
     <div class="searchInput">
-      <input type="search" placeholder="search">
-      <!-- <i class="fas fa-times-circle"></i> -->
+      <input placeholder="search">
+      <i class="fas fa-times-circle"></i>
     </div>
     <div class="searchBtn center">
       <button><i class="fas fa-search"></i></button>
@@ -13,6 +16,11 @@
 <script>
 export default {
   name: 'Search',
+  data() {
+    return {
+      prdType: ['熱門商品', '最新商品', '全部商品']
+    }
+  },
   props: {}
 }
 </script>
@@ -21,6 +29,14 @@ export default {
 .search {
   width: 100%;
   margin: 0 5px;
+}
+
+/* select */
+select {
+  border: 2px solid var(--searchBorderColor);
+  border-top-left-radius: 100px;
+  border-bottom-left-radius: 100px;
+  height: 35px;
 }
 
 /* input */
@@ -34,18 +50,18 @@ export default {
   width: 100%;
   height: 35px;
   border: 2px solid var(--searchBorderColor);
-  border-top-left-radius: 100px;
-  border-bottom-left-radius: 100px;
+  /* border-top-left-radius: 100px; */
+  /* border-bottom-left-radius: 100px; */
   padding: 0 8px 0 13px;
 }
 
-/* .searchInput svg {
+.searchInput svg {
   position: absolute;
   right: 7px;
-  top: 30px;
+  top: 9px;
   color: #cecbcb;
   font-size: 15px;
-} */
+}
 
 /* btn */
 .searchBtn button {
